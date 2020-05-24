60e6b3f8803854500f58312c9fc00589ff74f32f
Jacob Strong <ja.ha.strong@gmail.com>
2020-05-23 23:55:59 -0600

    fixxed

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,6 +73,10 @@ module.exports = {
       }
     }
   },
+  server: {
+    port: process.env.PORT || 3000, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  },
   /*
   ** Build configuration
   */
