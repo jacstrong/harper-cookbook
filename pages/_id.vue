18b9f8732d8b56d13d88233d4fbfa93a374e7825
Jacob Strong <ja.ha.strong@gmail.com>
2020-06-06 17:10:38 -0600

    change page title and description and load data in asyncData

--- pages/_id.vue
+++ pages/_id.vue
@@ -11,19 +11,37 @@ export default {
   components: {
     Recipe
   },
+  head () {
+    return {
+      title: this.recipe.name,
+      meta: [
+        { hid: 'description', name: 'description', content: `${this.recipe.name} by ${this.recipe.by}.` }
+      ]
+    }
+  },
   data: () => ({
     recipe: {}
   }),
-  created () {
-    console.log(this.$route.params)
-    this.$axios.$get(`/api/recipe/${this.$route.params.id}`)
+  asyncData ({$axios, params}) {
+    return $axios.get(`/api/recipe/${params.id}`)
       .then(res => {
-        console.log(res)
-        this.recipe = res
+        return { recipe: res.data }
       })
       .catch(err => {
         console.log(err)
+        return
       })
+  },
+  created () {
+    // console.log(this.$route.params)
+    // this.$axios.$get(`/api/recipe/${this.$route.params.id}`)
+    //   .then(res => {
+    //     console.log(res)
+    //     this.recipe = res
+    //   })
+    //   .catch(err => {
+    //     console.log(err)
+    //   })
   }
 }
 </script>
