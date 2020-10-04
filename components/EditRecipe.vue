c2849e9c04a0cf7549f7ee8c43f4be3f729cb98f
Jacob Strong <ja.ha.strong@gmail.com>
2020-10-03 22:32:21 -0600

    automatically updates an order to Employee order when Badger Screen Printing is selected as a customer

--- components/EditRecipe.vue
+++ components/EditRecipe.vue
@@ -182,10 +182,7 @@
       <div v-if="sub">
       <v-card>
         <v-card-text>
-          <v-row
-            v-for="(ingredient, i) in subIngredients"
-            :key="ingredient._id ? ingredient._id : ingredient.key"
-          >
+          <v-row>
             <v-col cols="12" sm="12" md="8">
               <v-text-field
                 label="Sub Recipe Name"
@@ -203,6 +200,11 @@
             <v-col cols="12" class="py-0">
               <hr>
             </v-col>
+          </v-row>
+          <v-row
+            v-for="(ingredient, i) in subIngredients"
+            :key="ingredient._id ? ingredient._id : ingredient.key"
+          >
             <v-col cols="6" sm="2">
               <v-text-field
                 label="amount"
